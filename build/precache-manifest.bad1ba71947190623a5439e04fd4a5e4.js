0aa7ba7dc65210adc597f0dbc86d59c56ad25bd9
drewbrad4 <drewbrad4@gmail.com>
2020-10-13 20:07:42 -0700

    fixed styling

---
+++ build/precache-manifest.bad1ba71947190623a5439e04fd4a5e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e457ed10f0e4983c424036b3432ae4",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "60a69ae72a30ee6a33a9",
+    "url": "/TicTacToe/static/css/main.b3e44326.chunk.css"
+  },
+  {
+    "revision": "571c278379e252e78321",
+    "url": "/TicTacToe/static/js/2.767edbe5.chunk.js"
+  },
+  {
+    "revision": "60a69ae72a30ee6a33a9",
+    "url": "/TicTacToe/static/js/main.054bc43a.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  }
+]);
